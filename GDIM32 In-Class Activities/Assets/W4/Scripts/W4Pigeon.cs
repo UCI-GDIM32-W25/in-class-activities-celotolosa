3f31aaa0e22357edc0baf7d5266b8e3da2844964
celotolosa <mttolosa@uci.edu>
2026-01-27 18:46:26 -0800

    update

--- GDIM32 In-Class Activities/Assets/W4/Scripts/W4Pigeon.cs
+++ GDIM32 In-Class Activities/Assets/W4/Scripts/W4Pigeon.cs
@@ -7,11 +7,12 @@ public class W4Pigeon : MonoBehaviour
 
     // REMOVE these references to other objects!
     // we're going to alert them via EVENT instead!!
-    [SerializeField] private W4Seagull[] _seagulls;
-    [SerializeField] private W4UI _ui;
-    [SerializeField] private W4VFX _vfx;
+
 
     // HERE, add an event to tell other objects that the pigeon coo'd!
+    public delegate void CooDelegate();
+    public event CooDelegate PigeonCoo;
+
 
     // don't change the code in this method!
     void Update()
@@ -34,15 +35,6 @@ public class W4Pigeon : MonoBehaviour
         // instead, fire your coo event!
         
         // tell seagulls
-        foreach(W4Seagull seagull in _seagulls)
-        {
-            seagull.HandlePigeonCoo();
-        }
-
-        // tell UI
-        _ui.HandlePigeonCoo();
-
-        // tell VFX
-        _vfx.HandlePigeonCoo();
+        PigeonCoo?.Invoke();
     }
 }
